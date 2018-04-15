c6259a884d39ec2f29f213bb01600fa50405f308
Gladson Santos <gladsonbruno16@gmail.com>
2018-04-15 10:02:58 -0300

    Validação de cadastro e login OK

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -15,6 +15,7 @@ export class LoginPage {
 
   public Email: string;
   public Password: string;
+  public camposVazios: string = "";
 
   constructor(
     public navCtrl: NavController,
@@ -31,25 +32,48 @@ export class LoginPage {
     const loading = this.loadingCtrl.create({
       duration: 500
     });
+    let _email = this.Email;
+    let _password = this.Password;
+    let _camposVazios = this.camposVazios;
+
 
     loading.onDidDismiss(() => {
-      // const alert = this.alertCtrl.create({
-      //   title: 'Logged in!',
-      //   subTitle: 'Thanks for logging in.',
-      //   buttons: ['Dismiss']
-      // });
-      // alert.present();
 
-      //this.navCtrl.setRoot(HomePage);
+      if(_email == "" || _email == undefined){
+        _camposVazios += "Preencha o campo Email\n";
+      }
+       if(_password == "" || _password == undefined){
+        _camposVazios += "Preencha o campo Senha";
+      }
+
+      if(_camposVazios == ""){
+        this.angularFireAuth.auth.signInWithEmailAndPassword(this.Email, this.Password).then(data => {
+          this.navCtrl.setRoot(HomePage);
+        }).catch(error => {
+          
+          var errorCode = error.code;
+          var errorMessage = error.message;
+  
+          if(errorMessage == "auth/invalid-email"){
+            alert("Email ou senha Incorretos");
+          }else if(errorMessage == "auth/wrong-password"){
+            alert("Email ou senha Incorretos");
+          }else if(errorMessage == "auth/user-not-found"){
+            alert("Usuário não Cadastrado");
+          }else if(errorMessage == "The email address is badly formatted."){
+            alert("Insira um Email válido");
+          }else{
+            alert("Usuário ou senha inválidos");
+          }
+  
+        });  
+      } else {
+        alert(_camposVazios);
+      }
+
     });
 
     loading.present();
-
-    this.angularFireAuth.auth.signInWithEmailAndPassword(this.Email, this.Password).then(function(AuthReturn){
-      console.log(AuthReturn);
-    }).catch(function(error){
-      console.log(error);
-    });
 
   }
 
--- src/pages/registrar/registrar.ts
+++ src/pages/registrar/registrar.ts
@@ -24,7 +24,8 @@ export class RegistrarPage {
 
   public Email: string;
   public Password: string;
-  public ThisPage = this;
+  public ConfirmarPassword: string;
+  public camposVazios: string = "";
 
   constructor(
     public navCtrl: NavController,
@@ -60,14 +61,51 @@ export class RegistrarPage {
       duration: 500
     });
 
+    let _email = this.Email;
+    let _password = this.Password;
+    let _confirmarPassword = this.ConfirmarPassword;
+    let _camposVazios = this.camposVazios;
+
     loading.onDidDismiss(() => {
-      this.angularFireAuth.auth.createUserWithEmailAndPassword(this.Email, this.Password).then(function(){
-        alert("Usuario Cadastrado com sucesso");
-        //NavController.prototype.push(LoginPage)
-      }).catch(function(error){
-        console.log(error);
-      });
-      this.navCtrl.push(LoginPage);
+  
+      if(_email == "" || _email == undefined){
+        _camposVazios += "Preencha o campo Email\n";
+      }
+       if(_password == "" || _password == undefined){
+        _camposVazios += "Preencha o campo Senha";
+      }
+      if(_password.length < 6){
+        _camposVazios += "O Campo senha deve conter no mínimo 6 caracteres"
+      }
+       if(_password != _confirmarPassword) {
+        _camposVazios += "O Campo senha e Confirmação de Senha devem conter o mesmo valor"
+      }
+
+      if(_camposVazios == ""){
+        this.angularFireAuth.auth.createUserWithEmailAndPassword(this.Email, this.Password).then(data => {
+          alert("Usuário Cadastrado com Sucesso!");
+          this.navCtrl.setRoot(LoginPage);
+        }).catch(error => {
+          
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          if(errorMessage == "auth/invalid-email"){
+           alert("Email Inválido");
+          }else if(errorMessage == "The email address is badly formatted."){
+   
+           alert("Insira um Email válido");
+   
+          }else if(errorMessage == "Password should be at least 6 characters"){
+   
+           alert("A senha precisa de no mínimo 6 caracteres");
+   
+          }
+  
+        });
+      } else {
+        alert(_camposVazios);
+      }
+
     });
 
     loading.present();
