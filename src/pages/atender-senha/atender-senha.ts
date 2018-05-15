e4184d70ae56bf7484ad7bb82ac18c406d6c3537
GladsonBruno <gladsonbruno16@gmail.com>
2018-05-15 20:29:28 -0300

    Atendimento e finalização de senha de acordo com a data atual

--- src/pages/atender-senha/atender-senha.ts
+++ src/pages/atender-senha/atender-senha.ts
@@ -37,24 +37,41 @@ export class AtenderSenhaPage {
   public tipoSenha: string;
   public Preferencial: boolean;
 
+  public today: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase) {
   }
 
   ngOnInit(){
+    let data = new Date();
+    let ano = data.getFullYear().toString();
+    let mes:any = data.getMonth() + 1;
+    mes = mes.toString();
+
+    if(mes.length == 1){
+        mes = "0" + mes;
+    }
+    var dia = data.getDate().toString();
+    if(dia.length == 1){
+        dia = "0" + dia;
+    }
+
+    this.today = dia + "/" + mes + "/" + ano;
+
     this.protocolosRDF = this.db.list("/reconhecimentoFirma", ref => ref.orderByChild('status').equalTo('Aberto')).snapshotChanges().map(arr => {
-      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) )
+      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) ).filter(i => i.date == this.today)
     });
 
     this.protocolosESC = this.db.list("/escritura", ref => ref.orderByChild('status').equalTo('Aberto')).snapshotChanges().map(arr => {
-      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) )
+      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) ).filter(i => i.date == this.today)
     });
 
     this.protocolosRGC = this.db.list("/registroCivil", ref => ref.orderByChild('status').equalTo('Aberto')).snapshotChanges().map(arr => {
-      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) )
+      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) ).filter(i => i.date == this.today)
     });
 
     this.protocolosPRC = this.db.list("/procuracao", ref => ref.orderByChild('status').equalTo('Aberto')).snapshotChanges().map(arr => {
-      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) )
+      return arr.map(snap => Object.assign(snap.payload.val(), { key: snap.key }) ).filter(i => i.date == this.today)
     });
 
   }
